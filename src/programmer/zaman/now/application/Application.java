beebb892531b1df529dfd919fbcf628dfcf5a35e
Andrey Sholinka <sholinka2111@gmail.com>
2023-03-17 14:49:38 +0700

    import

--- src/programmer/zaman/now/application/Application.java
+++ src/programmer/zaman/now/application/Application.java
@@ -1,6 +1,7 @@
 package programmer.zaman.now.application;
 
-import programmer.zaman.now.data.Product;
+
+import programmer.zaman.now.data.*;
 
 public class Application {
     public static void main(String[] args) {
@@ -8,5 +9,7 @@ public class Application {
 
         System.out.println(product.name);
         System.out.println(product.price);
+
+        Data data = new Data();
     }
 }
