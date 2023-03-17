650ce7540c3ad0fcc9e71a2748f0dfc905767651
Andrey Sholinka <sholinka2111@gmail.com>
2023-03-17 08:29:38 +0700

    method overriding

--- src/VicePresident.java
+++ src/VicePresident.java
@@ -1,2 +1,8 @@
 class VicePresident extends Manager{
+
+    // method overriding
+    void sayHello(String name){
+        System.out.println("Hi "+ name + ", My Name is VP "+ this.name);
+    }
+
 }
