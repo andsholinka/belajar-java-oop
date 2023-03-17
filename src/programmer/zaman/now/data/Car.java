7dc069e1b363549d5da9e7726513d6eae9a20633
Andrey Sholinka <sholinka2111@gmail.com>
2023-03-17 15:51:37 +0700

    interface inheritance

--- src/programmer/zaman/now/data/Car.java
+++ src/programmer/zaman/now/data/Car.java
@@ -1,9 +1,12 @@
 package programmer.zaman.now.data;
 
-public interface Car {
+public interface Car extends HasBrand, IsMaintenance{
     // interface mirip dengan abstract class, yg membedakan adalah semua method otomatis abstract
+    // buatlah kontrak menggunakan interface
 
     void drive();
 
     int getTire();
+
+
 }
