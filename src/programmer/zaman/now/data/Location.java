607765d2ea3e9b2ca38540d5825ce6a17b5e84d3
Andrey Sholinka <sholinka2111@gmail.com>
2023-03-17 14:55:19 +0700

    abstract class

---
+++ src/programmer/zaman/now/data/Location.java
@@ -0,0 +1,6 @@
+package programmer.zaman.now.data;
+
+public abstract class Location {
+    // abstract class tidak bisa dibuat object secara langsung, hanya bisa diturunkan
+    public String name;
+}
