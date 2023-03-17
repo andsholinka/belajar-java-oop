ea56a3d7c419d8a367db6cd6935fccb908aaec1f
Andrey Sholinka <sholinka2111@gmail.com>
2023-03-17 07:34:52 +0700

    fields/attributes/properties

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,7 @@
 public class Person {
+
+    // field/attribute/properties
+    String name;
+    String address;
+    final String country = "Indonesia";
 }
--- src/PersonApp.java
+++ src/PersonApp.java
@@ -2,13 +2,13 @@ public class PersonApp {
     public static void main(String[] args) {
 
         var person1 = new Person();
-        Person person2 = new Person();
-        Person person3;
-        person3 = new Person();
 
-        System.out.println(person1);
-        System.out.println(person2);
-        System.out.println(person3);
+        person1.name = "Andrey";
+        person1.address = "Bengkulu";
+
+        System.out.println(person1.name);
+        System.out.println(person1.address);
+        System.out.println(person1.country);
 
     }
 }
