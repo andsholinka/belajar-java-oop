b9c9fdc3717dea28e47cfaa6de3b4e2834b31ec5
Andrey Sholinka <sholinka2111@gmail.com>
2023-03-17 07:48:29 +0700

    contructor

--- src/Person.java
+++ src/Person.java
@@ -5,6 +5,12 @@ public class Person {
     String address;
     final String country = "Indonesia";
 
+    // contructor adalah method yg akan dipanggil ketika pertama kali object dibuat
+    Person(String paramName, String paramAddress){
+        name = paramName;
+        address = paramAddress;
+    }
+
     // menggunakan void karena tidak mengembalikan data apapun (return)
     void sayHello(String paramName){
         System.out.println("Hello " + paramName + ", My Name is " + name);
--- src/PersonApp.java
+++ src/PersonApp.java
@@ -1,15 +1,16 @@
 public class PersonApp {
     public static void main(String[] args) {
 
-        var person1 = new Person();
+        var person1 = new Person("Andrey", "Bengkulu");
 
-        person1.name = "Andrey";
-        person1.address = "Bengkulu";
+//        person1.name = "Andrey";
+//        person1.address = "Bengkulu";
 
-        System.out.println(person1.name);
-        System.out.println(person1.address);
-        System.out.println(person1.country);
+//        System.out.println(person1.name);
+//        System.out.println(person1.address);
+//        System.out.println(person1.country);
 
         person1.sayHello("Enzo");
+
     }
 }
