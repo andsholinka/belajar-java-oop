56750ef9ff95e0fc3136fda27339e1381beaeeb7
Andrey Sholinka <sholinka2111@gmail.com>
2023-03-20 06:14:40 +0700

    final class

---
+++ src/programmer/zaman/now/data/SocialMedia.java
@@ -0,0 +1,15 @@
+package programmer.zaman.now.data;
+
+public class SocialMedia {
+    String name;
+}
+
+final class Facebook extends SocialMedia {
+
+}
+
+// error - final class tidak bisa diturunkan
+
+//class FakeFacebook extends Facebook{
+//
+//}
