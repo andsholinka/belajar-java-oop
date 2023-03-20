91e109b42aa27d16fc2f93956ba21b8b2e9effd4
Andrey Sholinka <sholinka2111@gmail.com>
2023-03-20 07:54:28 +0700

    exception

---
+++ src/programmer/zaman/now/application/ValidationApp.java
@@ -0,0 +1,21 @@
+package programmer.zaman.now.application;
+
+import programmer.zaman.now.error.ValidationException;
+import programmer.zaman.now.util.ValidationUtil;
+
+public class ValidationApp {
+    public static void main(String[] args) {
+
+//        LoginRequest loginRequest = new LoginRequest(null, password);
+
+//        try {
+//            ValidationUtil.validate(loginRequest);
+//            System.out.println("Data valid");
+//        } catch (ValidationException | NullPointerException exception){
+//            System.out.println("Data tidak valid "+ exception.getMessage());
+//        } finnaly {
+                // contohnya untuk menutup connection
+//            System.out.println("Selalu di eksekusi");
+//        }
+    }
+}
---
+++ src/programmer/zaman/now/util/ValidationUtil.java
@@ -0,0 +1,14 @@
+package programmer.zaman.now.util;
+
+import programmer.zaman.now.error.ValidationException;
+
+public class ValidationUtil {
+
+//    public static void validate(LoginRequest loginRequest) throws ValidationException, NullPointerException {
+//        if (loginRequest.username() == null) {
+//            throw new NullPointerException("Username is null");
+//        } else if (loginRequest.usermane().isBlank()) {
+//            throw new ValidationException("username is blank");
+//        }
+//    }
+}
